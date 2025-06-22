c672b246d06c8dff407d446617f99ebeab7fed33
Geetha02 <geetharajamanickam1@gmail.com>
2025-06-22 14:33:08 +0100

    test file updated

--- test_app.py
+++ test_app.py
@@ -1,5 +1,7 @@
 from app import app
+
 def test_home():
-    response = app.test_client().get("/")
-    assert response.status_code == 200
-    assert response.data == "Hellow World!"
+    response=app.test_client().get("/")
+
+    assert response.status_code==200
+    assert response.data== b"Hello world!"
